04f04a4cf692cdefe905b50f78a38e875b4193df
tasypz <148768796+tasypz@users.noreply.github.com>
2023-12-11 19:58:43 +0700

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -16,12 +16,12 @@ export default function Navbar() {
               />
             </Link>
           </div>
-          <ul className="flex md:gap-20 sm:gap-10 gap-2 md:text-xl sm:text-base text-xs font-semibold text-sky-900 ">
-              <li className="hover:font-bold hover:text-slate-600"><Link href="/">Home</Link></li>
-              <li className="hover:font-bold hover:text-slate-600"><Link href="/about">About Us</Link></li>
-              <li className="hover:font-bold hover:text-slate-600"> <Link href="/services">Services</Link></li>
-              <li className="hover:font-bold hover:text-slate-600"><Link href="/team">Our Team</Link></li>
-          </ul>
+          <ul className="flex md:gap-20 sm:gap-10 gap-2 md:text-xl sm:text-base text-xs font-semibold text-sky-900">
+  <li className="hover:font-bold hover:text-slate-600"><Link href="/">Home</Link></li>
+  <li className="hover:font-bold hover:text-slate-600"><Link href="/about">About Us</Link></li>
+  <li className="hover:font-bold hover:text-slate-600"><Link href="/services">Services</Link></li>
+  <li className="hover:font-bold hover:text-slate-600"><Link href="/team">Our Team</Link></li>
+</ul>
         </div>
       </div>
     </>
