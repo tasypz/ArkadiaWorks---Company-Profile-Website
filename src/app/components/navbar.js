e33b9a458bce4ab7b683430531659eb531b8f813
tasypz <148768796+tasypz@users.noreply.github.com>
2023-12-11 19:54:20 +0700

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -17,18 +17,10 @@ export default function Navbar() {
             </Link>
           </div>
           <ul className="flex md:gap-20 sm:gap-10 gap-2 md:text-xl sm:text-base text-xs font-semibold text-sky-900 ">
-            <Link href="/">
-              <li className="hover:font-bold hover:text-slate-600">Home</li>
-            </Link>
-            <Link href="/about">
-              <li className="hover:font-bold hover:text-slate-600">About Us</li>
-            </Link>
-            <Link href="/services">
-              <li className="hover:font-bold hover:text-slate-600">Services</li>
-            </Link>
-            <Link href="/team">
-              <li className="hover:font-bold hover:text-slate-600">Our Team</li>
-            </Link>
+              <li className="hover:font-bold hover:text-slate-600"><Link href="/">Home</Link></li>
+              <li className="hover:font-bold hover:text-slate-600"><Link href="/about">About Us</Link></li>
+              <li className="hover:font-bold hover:text-slate-600"> <Link href="/services">Services</Link></li>
+              <li className="hover:font-bold hover:text-slate-600"><Link href="/team">Our Team</Link></li>
           </ul>
         </div>
       </div>
