cf1b7f22fde5f68892f82d26de719c2bef9a54b4
tasypz <148768796+tasypz@users.noreply.github.com>
2023-12-11 21:20:24 +0700

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -65,7 +65,7 @@ export default async function About() {
           <Image
             src={"/ArkadiaWorks.jpg"}
             alt="ArkadiaTeam"
-            width={900}
+            width={800}
             height={0}
             className="self-center text-center items-center origin-center object-center"
           />
